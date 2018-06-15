6a6264dcb9f8e2146f3dc9d66e3dada200f2974f
balachandra87 <balachandrapulavarthi@gmail.com>
2018-06-15 08:25:43 +0530

    lottery contract updated

--- Lottery.sol
+++ Lottery.sol
@@ -1,3 +1,4 @@
+pragma solidity ^0.4.20;
 contract Lottery {
     
     address public admin;
@@ -7,6 +8,10 @@ contract Lottery {
     uint8 public totalParticipants;
     
     bool public lotteryState;
+    
+    uint256 private randomCounter;
+    
+    mapping (address=>bool) entry;
     
     constructor () public {
         admin = msg.sender;
@@ -33,11 +38,14 @@ contract Lottery {
         lotteryState = false;
     }
     
-    function registerForLottery() payable public onlyTrueState {
+    function registerForLottery(uint256 _randomInput) payable public onlyTrueState {
         require(msg.value == 1 ether);
-        
+        require(msg.sender!=admin);
+        require(entry[msg.sender]==false);
         totalParticipants++;
         participants.push(msg.sender);
+        entry[msg.sender]= true;
+        randomCounter+= _randomInput;
         emit ParticipantRegistered(msg.sender,block.timestamp);
     }
     
@@ -47,7 +55,7 @@ contract Lottery {
         
         //uint8 winninerNumber = totalParticipants/2;
         
-        uint256 winninerNumber = uint256(keccak256(block.number+block.difficulty))%(totalParticipants-1);
+        uint256 winninerNumber = uint256(keccak256(block.number+block.difficulty+randomCounter))%(totalParticipants-1);
         
         participants[winninerNumber].transfer(totalParticipants*1 ether);
     }
